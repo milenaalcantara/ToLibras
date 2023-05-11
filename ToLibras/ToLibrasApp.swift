312a5899bc475dd0110af7aeb44bddd013be72f8
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-05-11 09:00:08 -0300

    Initial Commit

---
+++ ToLibras/ToLibrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToLibrasApp.swift
+//  ToLibras
+//
+//  Created by Milena Lima de Alcântara on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToLibrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
