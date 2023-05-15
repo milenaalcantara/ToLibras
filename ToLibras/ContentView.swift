c38cd89d7c216a63dad6d1b743e44cfdb4d3099c
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-05-15 08:39:47 -0300

    add access for camera and photo library

--- ToLibras/ContentView.swift
+++ ToLibras/ContentView.swift
@@ -6,16 +6,154 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
+    let model = ToLibras_HandPoses()
+    
+    @State var changeImage = false
+    @State var openSheet = false
+    @State var openCamera = false
+//    @State var openPhotos = false
+    @State var isPresentedDialog = false
+    @State var imageSelected = UIImage()
+    private var imageWidthSize: CGFloat = UIScreen.main.bounds.width * 0.9
+    
+    @State private var classificationLabel: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                isPresentedDialog = true
+            } label: {
+                ZStack {
+                    if changeImage {
+                        Image(uiImage: imageSelected)
+                            .resizable()
+                    } else {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .font(.largeTitle)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    VStack {
+                        Spacer()
+                        
+                        Text("Editar")
+                            .frame(width: imageWidthSize, height: 40)
+                            .background(.gray)
+                            .foregroundColor(.white)
+                            .font(.title2)
+                    }
+                }
+            }
+            .frame(width: imageWidthSize, height: imageWidthSize)
+            .border(.gray, width: 2)
+            .cornerRadius(9)
+            .confirmationDialog(
+                "Como você quer adicinar sua imagem?",
+                isPresented: $isPresentedDialog) {
+                    Button {
+                        classificationLabel = ""
+                        openSheet = true
+                        openCamera = true
+                        changeImage = true
+                    } label: {
+                        Text("Abrir a câmera")
+                    }
+                    
+                    Button {
+                        classificationLabel = ""
+                        openCamera = false
+                        openSheet = true
+                        changeImage = true
+                    } label: {
+                        Text("Escolher de Fotos")
+                    }
+                    
+                    Button("Cancel", role: .cancel) {
+                        isPresentedDialog = false
+                        print("Cancel")
+                    }
+                }
+                // task para setar false depois, todas as bools
+            
+            Button("Classificar") {
+                classifyImage(imageSelected)
+            }
+            .frame(width: imageWidthSize * 0.92)
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.green)
+            .cornerRadius(9)
+            
+            Text("\(classificationLabel)")
+                .padding()
+                .font(.body)
+
+            
+            Spacer()
         }
         .padding()
+        .sheet(isPresented: $openSheet) {
+            if openCamera {
+                ImagePicker(selectedImage: $imageSelected, sourceType: .camera)
+            } else {
+                ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
+            }
+            
+        }
+    }
+    
+    private func classifyImage(_ image: UIImage) {
+        // 1
+        guard let pose = preprocess(image: imageSelected) else {
+            return
+        }
+
+        // 3
+        let output = try? model.prediction(poses: pose)
+        
+        if let output = output {
+            let label = output.label
+            print(label)
+            self.classificationLabel = label
+        }
+        
+//        let output = try? model.prediction(image: buffer)
+//
+//        if let output = output {
+//            let results = output.classLabelProbs.sorted { $0.1 > $1.1 }
+//            let result = results.map { (key, value) in
+//                return "\(key) = \(String(format: "%.2f", value * 100))%"
+//            }.joined(separator: "\n")
+//
+//            self.classificationLabel = result
+//        }
+    }
+    
+    func preprocess(image: UIImage) -> MLMultiArray? {
+        let size = CGSize(width: 3, height: 21)
+        
+        
+        guard let pixels = image.resize(to: size).pixelData()?.map({ (Double($0) / 255.0 - 0.5) * 2 }) else {
+            return nil
+        }
+        
+        guard let array = try? MLMultiArray(shape: [1, 3, 21], dataType: .double) else {
+            return nil
+        }
+
+        let r = pixels.enumerated().filter { $0.offset % 4 == 0 }.map { $0.element }
+        let g = pixels.enumerated().filter { $0.offset % 4 == 1 }.map { $0.element }
+        let b = pixels.enumerated().filter { $0.offset % 4 == 2 }.map { $0.element }
+
+        let combination = r + g + b
+        for (index, element) in combination.enumerated() {
+            array[index] = NSNumber(value: element)
+        }
+        
+        return array
     }
 }
 
---
+++ ToLibras/Helpers/UIImage+Extension.swift
@@ -0,0 +1,86 @@
+//  UIImage+Extension.swift
+//
+//  Created by Moritz Philip Recke for Create with Swift on 10 February 2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func resizeImageTo(size: CGSize) -> UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+    
+     func convertToBuffer() -> CVPixelBuffer? {
+        
+        let attributes = [
+            kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+            kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
+        ] as CFDictionary
+        
+        var pixelBuffer: CVPixelBuffer?
+        
+        let status = CVPixelBufferCreate(
+            kCFAllocatorDefault, Int(self.size.width),
+            Int(self.size.height),
+            kCVPixelFormatType_32ARGB,
+            attributes,
+            &pixelBuffer)
+        
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let context = CGContext(
+            data: pixelData,
+            width: Int(self.size.width),
+            height: Int(self.size.height),
+            bitsPerComponent: 8,
+            bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
+            space: rgbColorSpace,
+            bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
+    public func resize(to newSize: CGSize) -> UIImage {
+                    UIGraphicsBeginImageContextWithOptions(CGSize(width: newSize.width, height: newSize.height), true, 1.0)
+            self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            
+            return resizedImage
+    }
+    public func pixelData() -> [UInt8]? {
+            let dataSize = size.width * size.height * 4
+            var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
+            let colorSpace = CGColorSpaceCreateDeviceRGB()
+            let context = CGContext(data: &pixelData, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: 4 * Int(size.width), space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue)
+            
+            guard let cgImage = self.cgImage else { return nil }
+            context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            
+            return pixelData
+    }
+
+}
